7f10724708f2d00e8b404d51894923bd57c8a2df
wozniakm83 <wozniakmichal83@gmail.com>
2017-12-10 08:43:30 +0100

    Refactoring - part I

--- fixture/group.py
+++ fixture/group.py
@@ -3,44 +3,61 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def create(self, group):
+    def change_field_value(self, field_name, text):
         wd = self.app.wd
-        self.app.goto.groups_page()
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def select_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def init_group_creation(self):
+        wd = self.app.wd
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def submit_group_creation(self):
+        wd = self.app.wd
         wd.find_element_by_name("submit").click()
+
+    def init_group_modification(self):
+        wd = self.app.wd
+        wd.find_element_by_name("edit").click()
+
+    def submit_group_modification(self):
+        wd = self.app.wd
+        wd.find_element_by_name("update").click()
+
+    def submit_group_deletion(self):
+        wd = self.app.wd
+        wd.find_element_by_name("delete").click()
+
+    def create(self, group):
+        self.app.goto.groups_page()
+        self.init_group_modification()
+        self.fill_group_form(group)
+        self.submit_group_creation()
         self.return_to_groups_page()
 
     def modify(self, group):
-        wd = self.app.wd
         self.app.goto.groups_page()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
+        self.select_group()
+        self.init_group_modification()
+        self.fill_group_form(group)
+        self.submit_group_modification()
         self.return_to_groups_page()
 
     def delete(self):
-        wd = self.app.wd
         self.app.goto.groups_page()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("delete").click()
+        self.select_group()
+        self.submit_group_deletion()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,19 @@
 from model.group import Group
 
 
-def test_modify_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="Test2", header="new header", footer="new footer"))
+    app.group.modify(Group(name="new name"))
     app.session.logout()
+
+
+# def test_modify_group_header(app):
+#     app.session.login(username="admin", password="secret")
+#     app.group.modify(Group(header="new header"))
+#     app.session.logout()
+#
+#
+# def test_modify_group_footer(app):
+#     app.session.login(username="admin", password="secret")
+#     app.group.modify(Group(footer="new footer"))
+#     app.session.logout()
